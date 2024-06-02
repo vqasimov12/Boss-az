9463919817f1572f56cdc299ea884228b1382e5d
Vaqif <158089331+vqasimov12@users.noreply.github.com>
2024-06-02 12:23:30 +0400

    last version

--- Boss.az/Main.cs
+++ Boss.az/Main.cs
@@ -496,7 +496,7 @@ public class Main
             if (HtmlBody)
                 mail.IsBodyHtml = true;
             smtp.Port = 587;
-            smtp.Credentials = new NetworkCredential("qasimov.vaqif512@gmail.com", "mnnc lpwi nzua ocjg");
+            smtp.Credentials = new NetworkCredential("example@gmail.com", "key");
             smtp.EnableSsl = true;
             smtp.Send(mail);
             AddLog($"Mail sent to {_destinationMail} -> ");
